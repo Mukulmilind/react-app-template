a64812d9ef00d8b46878609e1d17afd69342a137
Mishra <mukulmilind23@gmail.com>
2020-02-24 13:15:43 +0530

    React App template for starting a react application

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'; 
+import profile from '../assets/profile.jpg';
+import '../index.css';
+
+class App extends Component {
+    render() {
+         return (
+            <div>React Application</div>
+        )
+    }
+}
+
+export default App;
